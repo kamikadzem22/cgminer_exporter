9220cb5d7c9fb297f8085a3345b003982a5775cc
Andrey Masyanov <masyanovm22@gmail.com>
2021-06-01 21:43:22 +0300

    rewrite to fastapi

---
+++ cgminer_explorter.py
@@ -0,0 +1,127 @@
+# Copyright 2013 Setkeh Mkfr
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.  See COPYING for more details.
+
+#Short Python Example for connecting to The Cgminer API
+#Written By: setkeh <https://github.com/setkeh>
+#Thanks to Jezzz for all his Support.
+#NOTE: When adding a param with a pipe | in bash or ZSH you must wrap the arg in quotes
+#E.G "pga|0"
+import uvicorn
+import socket
+import os
+import json
+import sys
+import pprint
+import datetime
+import requests
+import traceback
+import requests
+from sys import exc_info
+pp = pprint.PrettyPrinter(indent=4)
+
+from typing import Optional
+import asyncio
+from fastapi import FastAPI, Response
+from metric import *
+app = FastAPI()
+statusdata = {}
+    
+STORAGE = {}
+
+AVAILABLE_COMMANDS = [ 'stats', 'version', 'pools', 'summary', 'devs' ]
+
+export_metrics = {
+    'summary': metric_summary,
+    'pools': metric_pool,
+    'stats': metric_stats
+}
+
+STORAGE = {}
+try:
+    with open('C:\\Monitoring\\creds.json') as f:
+        CGI_LOGIN, CGI_PASSWORD = json.loads(f.read()).values()
+except:
+    CGI_LOGIN = None
+    CGI_PASSWORD = None
+
+def fetch_network_info(target):
+    if CGI_LOGIN == None:
+        STORAGE[target] = ("", "")
+    if target not in STORAGE:
+        url = "http://%s/cgi-bin/get_network_info.cgi"%target
+        data = json.loads(requests.get(url, auth=requests.auth.HTTPDigestAuth(CGI_LOGIN, CGI_PASSWORD)).text)
+        STORAGE[target] = (data["conf_hostname"], data["macaddr"]) 
+
+    return STORAGE[target]
+
+async def tcp_client(ip, command):
+    data = b''
+    reader, writer = await asyncio.open_connection(ip, 4028)
+
+    writer.write(('{"command":"%s"}' % command).encode())
+    await writer.drain()
+
+    while True:
+        chunk = await reader.read(100)
+        if not chunk:
+            break
+        data += chunk
+    return json.loads(data.decode().replace('\x00', '').replace('}{', '},{'))
+
+async def fetch_metrics(ip, command):
+    data = await tcp_client(ip, command)
+    return (command, data)
+
+@app.get("/")
+def index():
+    return Response("Use /metrics with ?target=IP\n")
+
+@app.get("/api/{cmd}")
+async def get_stats(cmd:str, target: str):
+    if cmd in AVAILABLE_COMMANDS:
+        text = await tcp_client(target, cmd)
+        return text
+    return {'error': 'unknown command'}
+
+async def parse_tags(target, metricdata):
+    if 'CGMiner' in metricdata['version']['VERSION'][0]:
+        tags = 'instance="%s",cgminer_version="%s",api_version="%s",type="%s",miner="%s"'%(target,metricdata['version']['VERSION'][0]['CGMiner'],metricdata['version']['VERSION'][0]['API'],metricdata['version']['VERSION'][0]['Type'],metricdata['version']['VERSION'][0]['Miner'])
+    elif 'BMMiner' in metricdata['version']['VERSION'][0]:
+        tags = 'instance="%s",bmminer_version="%s",api_version="%s",type="%s",miner="%s"'%(target,metricdata['version']['VERSION'][0]['BMMiner'],metricdata['version']['VERSION'][0]['API'],metricdata['version']['VERSION'][0]['Type'],metricdata['version']['VERSION'][0]['Miner'])
+    else:
+        tags = 'instance="%s",api_version="%s",type="%s",miner="%s"'%(target, metricdata['version']['VERSION'][0]['API'],metricdata['version']['VERSION'][0]['Type'],metricdata['version']['VERSION'][0]['Miner'])
+    
+    if target not in STORAGE:
+        hostname, mac = fetch_network_info(target)
+    else:
+        hostname, mac = STORAGE[target]
+    tags+=',hostname="%s",mac="%s"'%(hostname, mac)
+    return tags
+
+@app.get("/metrics")
+async def get_metrics(target: str):
+    res = "#CGMiner metrics export\n"
+
+    metric_data = dict(await asyncio.gather(
+        *[fetch_metrics(target, cmd) for cmd in AVAILABLE_COMMANDS]
+    ))
+    
+    tags = await parse_tags(target, metric_data)
+
+
+    
+    res+= "\n".join(
+        await asyncio.gather(
+            *[export_metrics[cmd](metric_data[cmd], tags) for cmd in export_metrics]
+        )
+    )
+
+    return Response(res)
+    
+
+if __name__ == "__main__":
+    uvicorn.run("cgminer_exporter:app", host="0.0.0.0", port=9154, log_level="info")
---
+++ metric.py
@@ -0,0 +1,69 @@
+import datetime
+async def metric_pool(data, tags):
+	string = "# Pools Data\n"
+	string += "cgminer_pool_count{%s} %s\n"%(tags, len(data['POOLS']))
+	for pool in data['POOLS']:
+		localtags = 'pool="%s",url="%s",stratum_url="%s",%s'%(pool['POOL'], pool['URL'], pool['Stratum URL'], tags)
+		string += 'cgminer_pool_diff_accepted{%s} %s\n'%(localtags, pool['Difficulty Accepted'])
+		string += 'cgminer_pool_rejected{%s} %s\n'%(localtags, pool['Difficulty Accepted'])
+		string += 'cgminer_pool_diff_rejected{%s} %s\n'%(localtags, pool['Difficulty Rejected'])
+		string += 'cgminer_pool_stale{%s} %s\n'%(localtags, pool['Stale'])
+		try:
+			[hr, mn, ss] = [int(x) for x in pool['Last Share Time'].split(':')]
+			sharetime = datetime.timedelta(hours=hr, minutes=mn, seconds=ss).seconds
+		except:
+			sharetime = 0
+		string += 'cgminer_pool_last_share{%s} %s\n'%(localtags, sharetime)
+		string += 'cgminer_pool_getworks{%s} %s\n'%(localtags, pool['Getworks'])
+		string += 'cgminer_pool_last_diff{%s} %s\n'%(localtags, pool['Last Share Difficulty'])
+		if pool['Status'] == "Alive":
+			status = 1
+		else:
+			status = 0
+		string += 'cgminer_pool_status{%s} %s\n'%(localtags, status)
+		if pool['Stratum Active']:
+			active = 1
+		else:
+			active = 0
+		string += 'cgminer_pool_stratum_active{%s} %s\n'%(localtags, active)
+	return (string)
+
+async def metric_summary(data, tags):
+	string = "#Pool Summary\n"
+	localtags = tags
+	string += 'cgminer_summary_rejected{%s} %s\n'%(localtags, data['SUMMARY'][0]['Rejected'])
+	string += 'cgminer_summary_found_blocks{%s} %s\n'%(localtags, data['SUMMARY'][0]['Found Blocks'])
+	string += 'cgminer_summary_elapsed{%s} %s\n'%(localtags, data['SUMMARY'][0]['Elapsed'])
+	string += 'cgminer_summary_hardware_errors{%s} %s\n'%(localtags, data['SUMMARY'][0]['Hardware Errors'])
+	string += 'cgminer_summary_total_mh{%s} %s\n'%(localtags, data['SUMMARY'][0]['Total MH'])
+	string += 'cgminer_summary_ghs_average{%s} %s\n'%(localtags, data['SUMMARY'][0]['GHS av'])
+	string += 'cgminer_summary_ghs_5s{%s} %s\n'%(localtags, data['SUMMARY'][0]['GHS 5s'])
+
+	return (string)
+
+async def metric_stats(data, tags):
+	string = "# Stats\n"
+	statdata = data['STATS'][1]
+	localtags = '%s'%(tags)
+	for entry in statdata:
+		if 'temp' in entry:
+			tempnum = entry.replace("temp","")
+			string += 'cgminer_stats_temp{temp="%s",%s} %s\n'%(tempnum, localtags, statdata['temp%s'%(tempnum)])
+		if 'chain_hw' in entry:
+			chainnum = entry.replace("chain_hw","")
+			if statdata['chain_rate%s'%(chainnum)]:
+				string += 'cgminer_stats_chain_rate{chain="%s",%s} %s\n'%(chainnum, localtags, statdata['chain_rate%s'%(chainnum)])
+			else:
+				string += 'cgminer_stats_chain_rate{chain="%s",%s} %s\n'%(chainnum, localtags, 0)
+			string += 'cgminer_stats_chain_acn{chain="%s",%s} %s\n'%(chainnum, localtags, statdata['chain_acn%s'%(chainnum)])
+			string += 'cgminer_stats_chain_hw{chain="%s",%s} %s\n'%(chainnum, localtags, statdata['chain_hw%s'%(chainnum)])
+		if 'fan' in entry and entry != "manual_fan_mode":
+			fannum = entry.replace("fan","")
+			string += 'cgminer_stats_fan{fan="%s",%s} %s\n'%(fannum, localtags, statdata['fan%s'%(fannum)])
+		if 'freq_avg' in entry:
+			freqnum = entry.replace("freq_avg","")
+			string += 'cgminer_stats_freq{freq="%s",%s} %s\n'%(freqnum, localtags, statdata['freq_avg%s'%(freqnum)])
+
+	string += 'cgminer_stats_frequency{%s} %s\n'%(localtags, statdata['frequency'])
+
+	return (string)
