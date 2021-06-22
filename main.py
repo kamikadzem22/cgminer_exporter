219dceec1985c0dca04578f0d3734a9b0d17d589
Andrey Masyanov <masyanovm22@gmail.com>
2021-06-22 15:49:22 +0300

    add second worker

--- main.py
+++ main.py
@@ -193,4 +193,4 @@ async def get_metrics(target: str):
     
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9154, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=9154, log_level="info", workers=2)
